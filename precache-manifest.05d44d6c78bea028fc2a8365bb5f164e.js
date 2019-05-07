4f22298d6420f893946cd8f7aef62767fcb2d235
Kyle Robert <notoriouskro@gmail.com>
2019-05-07 16:56:41 -0700

    Updates

---
+++ precache-manifest.05d44d6c78bea028fc2a8365bb5f164e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad925aef5b5843741db336188b3e421",
+    "url": "/GOTclickyGame/index.html"
+  },
+  {
+    "revision": "7e70c73fc9619b05fe5d",
+    "url": "/GOTclickyGame/static/css/main.91b2af37.chunk.css"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/GOTclickyGame/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "7e70c73fc9619b05fe5d",
+    "url": "/GOTclickyGame/static/js/main.8601f32f.chunk.js"
+  },
+  {
+    "revision": "fc951665227f1d6cf92d",
+    "url": "/GOTclickyGame/static/js/runtime~main.3f70faa7.js"
+  }
+]);
